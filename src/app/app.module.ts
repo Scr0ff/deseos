8ce2c0f59b981bd7df924b30437e7f4e0334549c
dapo11000 <dakota152>
2018-12-10 15:48:11 -0500

    Agregando listas de deseos

---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+
+import { TabsPage } from '../pages/tabs/tabs';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+//Components
+import { PendientesComponent } from '../pages/pendientes/pendientes.component';
+import { TerminadosComponent } from '../pages/terminados/terminados.component';
+
+//Servicios
+import { ListaDeseosService } from './services/lista-deseos.service';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    PendientesComponent,
+    TerminadosComponent,
+    TabsPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    PendientesComponent,
+    TerminadosComponent,
+    TabsPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    ListaDeseosService,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
